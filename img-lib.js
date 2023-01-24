f4f0fa1fb5c21ddf8297e58f2890371035347886
Cal Schwick <35741152+CalSch@users.noreply.github.com>
2023-01-24 12:59:25 -0600

    added lerp,lerpColor, and more color constants

--- img-lib.js
+++ img-lib.js
@@ -54,8 +54,22 @@ function lum(v) {
 	return new Color(v, v, v)
 }
 
-const WHITE=new Color(255,255,255)
-const BLACK=new Color(0,0,0)
-const RED=  new Color(255,0,0)
-const GREEN=new Color(0,255,0)
-const BLUE= new Color(0,0,255)
+const WHITE=  new Color( 255, 255, 255 )
+const BLACK=  new Color( 0  , 0  , 0   )
+const RED=    new Color( 255, 0  , 0   )
+const GREEN=  new Color( 0  , 255, 0   )
+const BLUE=   new Color( 0  , 0  , 255 )
+const YELLOW= new Color( 255, 255, 0   )
+const CYAN=   new Color( 0  , 255, 255 )
+const MAGENTA=new Color( 255, 0  , 255 )
+
+function lerp(a,b,t) {
+	return a*(1-t)+b*t
+}
+function lerpColor(a,b,t) {
+	return new Color(
+		lerp(a.r,b.r,t),
+		lerp(a.g,b.g,t),
+		lerp(a.b,b.b,t),
+	)
+}
--- lib.d.ts
+++ lib.d.ts
@@ -20,8 +20,22 @@ declare const GREEN:Color;
 declare const BLUE:Color;
 
 declare function pix(x:number, y:number): Color;
+
+/**
+ * Returns a color from HSL values
+ * @param h The hue value from 0-360
+ * @param s The saturation value from 0-1
+ * @param l The lightness value from 0-1
+ */
 declare function hsl(h:number, s:number, l:number): Color;
+/**
+ * Returns a shade of grey based on the lightness
+ * @param v Lightess (0-255)
+ */
 declare function lum(v:number): Color;
 
 declare function log(at:string,message:string);
 declare function clear();
+
+declare function lerp(a:number,b:number,t:number):number;
+declare function lerpColor(a:Color,b:Color,t:number):Color;
